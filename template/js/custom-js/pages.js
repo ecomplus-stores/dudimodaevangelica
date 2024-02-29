774ee8f18c093118e72e95565fac7800b5583b50
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2024-02-29 17:10:04 -0300

    chore(pages.js): set redirect to cart when add item in cart

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,7 @@
 // Add your custom JavaScript for storefront pages here.
+import { isMobile } from '@ecomplus/storefront-twbs'
+if (isMobile) {
+    ecomCart.on('addItem', () => { 
+        window.location = '/app/#/cart/' 
+    })
+}
